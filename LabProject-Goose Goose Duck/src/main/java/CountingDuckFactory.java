8494abc44f1733201177150a16cb5113b1ee4e54
Tawfiq-E-Elahi <95173135+TEEShakkhor@users.noreply.github.com>
2024-06-03 21:54:13 +0600

    Delete LabProject-Goose Goose Duck directory

--- LabProject-Goose Goose Duck/src/main/java/CountingDuckFactory.java
+++
@@ -1,6 +0,0 @@
-public class CountingDuckFactory {
-    public Quackable createDuck(String type) {
-        Quackable duck = new DuckFactory().createDuck(type);
-        return new QuackCounter(duck);
-    }
-}
--- LabProject-Goose Goose Duck/src/main/java/Quackologist.java
+++
@@ -1,5 +0,0 @@
-public class Quackologist implements Observer{
-    public void update(Quackable duck) {
-        System.out.println("Quackologist: " + duck.toString() + " just quacked.");
-    }
-}
